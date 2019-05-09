467c7eecdbc5d556d8cedf3f3ba9791a41f11125
Nikolas Bernade <nick@trueframe.com>
2019-05-09 16:32:59 -0400

    Re-do token scanner

--- src/application/util/FirebaseSQL.ts
+++ src/application/util/FirebaseSQL.ts
@@ -1,14 +1,20 @@
 import * as firebase from "firebase-admin";
+import * as startOfDay from "date-fns/start_of_day";
+import * as endOfDay from "date-fns/end_of_day";
 
 enum KEYWORDS {
-    SELECT  = "SELECT",
-    UPDATE  = "UPDATE",
-    DELETE  = "DELETE",
-    FROM    = "FROM",
-    WHERE   = "WHERE",
-    AND     = "AND",
-    OR      = "OR",
-    ">"     = ">"
+    CREATE      = "create",
+    SELECT      = "select",
+    INSERT      = "insert",
+    UPDATE      = "update",
+    DELETE      = "delete",
+    FROM        = "from",
+    WHERE       = "where",
+    AND         = "and",
+    OR          = "or",
+    ">"         = ">",
+    BETWEEN     = "between",
+    DUPLICATE   = "duplicate"
 }
 
 const keywordsArray: string[] = [
@@ -22,13 +28,82 @@ const keywordsArray: string[] = [
     KEYWORDS[">"]
 ];
 
+const tokenToType = (tokenValue: string, tokens: Tokens) => {
+
+    let valAsNumber = parseFloat(tokenValue);
+    let valAsDate = ISODateStringToDate(tokenValue);
+
+    if(tokenValue === "null") {
+
+        return null;
+
+    } else if(valAsDate) {
+
+        return valAsDate;
+
+    } else if(!isNaN(valAsNumber)) {
+
+        return valAsNumber;
+
+    } else if(["true", "false"].indexOf(tokenValue) !== -1) {
+
+        return tokenValue === "true";
+
+    } else if(tokenValue.startsWith("\"")) {
+
+        tokens.stepBackwards();
+        tokens.setOffset(tokens.getOffset() + 1);
+
+        return tokens.getNextToken("\"");
+
+    } else if(tokenValue.startsWith("{")) {
+
+        const result: any = {};
+
+        tokens.stepBackwards();
+        tokens.setOffset(tokens.getOffset() + 1);
+
+        const keyValues = tokens.getNextToken("}");
+
+        for(const token of keyValues.split(" ")) {
+            const s = token.split(":");
+
+            const key = s[0], val = s[1];
+
+            if(!key || !val) {
+                throw new Error("");
+            }
+
+            result[key] = tokenToType(val, new Tokens(val));
+        }
+
+        return result;
+
+    } else {
+        throw Error("Unknown value type.");
+    }
+};
+
+const ISODateStringToDate = (str: string) => {
+    const regexp = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
+
+    if(!str.match(regexp)) {
+        return null;
+    }
+
+    const s = str.split("-");
+
+    return new Date(parseInt(s[0]), parseInt(s[1]) - 1, parseInt(s[2]));
+};
+
 class Tokens {
 
-    private tokens: string[];
-    private offset;
+    private tokenString: string;
+    private offset: number;
+    private previousToken: string;
 
-    constructor(s: string) {
-        this.tokens = s.split(" ").filter(s => { return s.length });
+    constructor(tokenString: string) {
+        this.tokenString = tokenString.replace(/\s\s+/g, ' ');
         this.offset = 0;
     }
 
@@ -40,74 +115,97 @@ class Tokens {
         return this.offset;
     }
 
-    public getNextToken(asUppercase: boolean = false) {
+    public stepBackwards() {
 
-        const token = this.tokens[this.offset];
-
-        if(!token) {
-            throw Error("Exhausted token array.");
+        if(!this.previousToken) {
+            return;
         }
 
-        this.offset += 1; // Start from the next index
+        this.offset = this.offset - this.previousToken.length - 1;
+        this.previousToken = null;
+    }
 
-        return asUppercase ? token.toUpperCase() : token;
+    public getNextToken(stopCharacter?: string) {
+
+        const array = [];
+
+        for(let x = this.offset; x < this.tokenString.length; x++) {
+
+            const c = this.tokenString[x];
+
+            if(stopCharacter) {
+
+                if(c.charCodeAt(0) === stopCharacter.charCodeAt(0)) {
+                    this.offset += array.length + 2;
+                    break;
+                }
+
+            } else if(c.charCodeAt(0) === 32) {
+                this.offset += array.length + 1;
+                break;
+
+            }
+
+            array.push(c);
+        }
+
+        //this.offset += array.length + 1;
+
+        if(!array.length) {
+            throw new Error("Exhausted token array.");
+        }
+
+        const token = array.join("").trim();
+
+        this.previousToken = token;
+
+        return token;
     }
 }
 
 export default async function (query: string, firestore: firebase.firestore.Firestore) {
 
-    console.info(query);
-
     const tokens = new Tokens(query);
 
-    let fields = null, collection = null;
+    let fields = null, collection = null, duplicateDocument = null;
 
     // First token should be a clause that identifies the operation to perform.
-    switch (tokens.getNextToken(true)) {
+    switch (tokens.getNextToken().toLowerCase()) {
         case KEYWORDS.SELECT:
 
-            let selectFields = tokens.getNextToken();
+            let selectFields = [];
 
-            // An asterisk denotes all fields.
-            if(selectFields === '*') {
+            let nextToken = tokens.getNextToken();
+
+            while(keywordsArray.indexOf(nextToken.toLowerCase()) === -1) {
+
+                selectFields.push(nextToken);
+
+                nextToken = tokens.getNextToken();
+            }
+
+            if(selectFields.filter(field => { return field === "*" }).length) {
+
+                if(selectFields.length !== 1) {
+                    throw new Error("");
+                }
 
                 fields = [];
 
             } else {
 
-                const tmp = [];
+                fields = selectFields;
 
-                for(let x = tokens.getOffset();;x++) {
-                    console.info("field", selectFields);
-                    if(keywordsArray.indexOf(selectFields) !== -1) {
-                        tokens.setOffset(x - 1);
-                        break;
-                    }
-
-                    if(selectFields.length) {
-                        tmp.push(selectFields);
-                    }
-
-                    selectFields = tokens.getNextToken();
-                }
-
-                // Fields are enclosed by parenthesis. This makes it easier to parse.
-
-
-
-                //const sub = tmp.join(" ");
-
-                for(const t of tmp) {
-                    // TODO
-                }
-
-                fields = tmp;
             }
+
+            tokens.stepBackwards();
 
             break;
         case KEYWORDS.UPDATE:
 
             const collection = tokens[1];
+
+
 
             break;
         case KEYWORDS.DELETE:
@@ -115,11 +213,33 @@ export default async function (query: string, firestore: firebase.firestore.Fire
 
 
             break;
+
+        case KEYWORDS.DUPLICATE:
+
+            duplicateDocument = tokens.getNextToken();
+
+            break;
+
+        case KEYWORDS.INSERT: {
+
+            tokens.setOffset(tokens.getOffset() + 1);
+
+            const value = tokens.getNextToken("}");
+
+            const o = tokens.getNextToken();
+
+            const collection = tokens.getNextToken();
+
+            console.info(value, o, collection);
+
+            return;
+        }
+
         default:
             throw new Error("Unknown TODO");
     }
 
-    switch (tokens.getNextToken(true)) {
+    switch (tokens.getNextToken().toLowerCase()) {
         case KEYWORDS.FROM:
 
             collection = tokens.getNextToken();
@@ -135,28 +255,72 @@ export default async function (query: string, firestore: firebase.firestore.Fire
         value: any;
     }[] = [];
 
+    let whereUsed = false;
+    let orderBy: { field: string, startDate: Date, endDate: Date } = null;
+
     while(true) {
         let nextOptionalToken;
 
         try {
-            nextOptionalToken = tokens.getNextToken(true);
+            nextOptionalToken = tokens.getNextToken();
         } catch (e) {
+            console.info(constraints);
             break;
         }
 
-        switch (nextOptionalToken) {
+        switch (nextOptionalToken.toLowerCase()) {
+            case KEYWORDS.AND:
             case KEYWORDS.WHERE:
-                const field = tokens.getNextToken(), operator = tokens.getNextToken(), value = tokens.getNextToken();
+                const field = tokens.getNextToken(), operator = tokens.getNextToken().toLowerCase();
+
+                if(nextOptionalToken === KEYWORDS.WHERE) {
+                    whereUsed = true;
+                } else if(nextOptionalToken === KEYWORDS.AND && !whereUsed) {
+                    throw Error("");
+                }
+
+                if(operator === KEYWORDS.BETWEEN) {
+                    let left: any = tokens.getNextToken(), right: any = tokens.getNextToken();
+
+                    left = ISODateStringToDate(left);
+                    right = ISODateStringToDate(right);
+
+                    if(!left || !right) {
+                        throw Error("");
+                    }
+
+                    orderBy = { field, startDate: startOfDay(left), endDate: endOfDay(right) };
+
+                    continue;
+                }
+
+                let value = tokenToType(tokens.getNextToken(), tokens);
+
+                console.info("Constraint value", value);
 
                 constraints.push({ field, operator, value });
 
                 break;
             default:
-                throw Error("Uknown TODO");
+                throw Error("Unknown TODO");
         }
     }
 
     let fireQuery: firebase.firestore.Query = firestore.collection(collection);
+
+    if(duplicateDocument) {
+        const doc = await (fireQuery as firebase.firestore.CollectionReference).doc(duplicateDocument).get();
+
+        if(!doc.exists) {
+            throw Error("TODO")
+        }
+
+        const ref: firebase.firestore.DocumentReference = firestore.collection(collection).doc();
+
+        ref.set(doc.data());
+
+        return null; // TODO
+    }
 
     if(fields.length) {
         fireQuery = fireQuery.select(...fields);
@@ -166,6 +330,10 @@ export default async function (query: string, firestore: firebase.firestore.Fire
         fireQuery = fireQuery.where(constraint.field, constraint.operator, constraint.value);
     });
 
-    // TODO configure limir
+    if(orderBy) {
+        fireQuery = fireQuery.orderBy(orderBy.field).startAt(orderBy.startDate).endAt(orderBy.endDate);
+    }
+
+    // TODO configure limit
     return fireQuery.limit(25).get();
 };
