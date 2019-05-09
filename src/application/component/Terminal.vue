7f239f9c0e868d03530134496973183930e90f3c
Nikolas Bernade <nick@trueframe.com>
2019-05-09 16:32:40 -0400

    Make button work

--- src/application/component/Terminal.vue
+++ src/application/component/Terminal.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="terminal-container">
         <textarea v-on:keypress="onTextAreaKeyPress" v-model="queryModel"></textarea>
-        <button class="button">Submit</button>
+        <button v-bind:disabled="isDisabled" v-on:click="submitQuery" class="button">
+            {{ isDisabled ? "Loading" : "Submit" }}
+        </button>
     </div>
 </template>
 
 <script lang="ts">
     export default {
         name: "termInput",
+        props: [ "isDisabled" ],
         data: () => {
             return {
                 queryModel: ""
@@ -15,12 +18,21 @@
         },
         methods: {
 
+            submitQuery: function() {
+
+                if(this.isDisabled) {
+                    return;
+                }
+
+                this.$emit("querySubmit", this.queryModel);
+            },
+
             onTextAreaKeyPress: function(e) {
                 const { keyCode } = e;
 
                 if(keyCode === 13) {
                     e.preventDefault();
-                    this.$emit("querySubmit", this.queryModel);
+                    this.submitQuery();
                 }
             }
 
